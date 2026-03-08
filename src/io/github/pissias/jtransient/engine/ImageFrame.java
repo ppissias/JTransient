f362cebba58548eaa5806edf2079fecdb5f0d1c3
Petros Pissias <petrospis@gmail.com>
2026-03-08 09:17:59 +0200

    init project

---
+++ src/io/github/pissias/jtransient/engine/ImageFrame.java
@@ -0,0 +1,13 @@
+package io.github.pissias.jtransient.engine;
+
+public class ImageFrame {
+    public final int sequenceIndex;
+    public final String identifier; // Usually the filename
+    public final short[][] pixelData;
+
+    public ImageFrame(int sequenceIndex, String identifier, short[][] pixelData) {
+        this.sequenceIndex = sequenceIndex;
+        this.identifier = identifier;
+        this.pixelData = pixelData;
+    }
+}
