81484c876ef099db916075d8f96dd453556e0caa
Petros Pissias <petrospis@gmail.com>
2026-03-19 20:13:50 +0200

    added time information

--- src/main/java/io/github/ppissias/jtransient/engine/ImageFrame.java
+++ src/main/java/io/github/ppissias/jtransient/engine/ImageFrame.java
@@ -13,10 +13,12 @@ public class ImageFrame {
     public final int sequenceIndex;
     public final String identifier; // Usually the filename
     public final short[][] pixelData;
+    public final long timestamp;
 
-    public ImageFrame(int sequenceIndex, String identifier, short[][] pixelData) {
+    public ImageFrame(int sequenceIndex, String identifier, short[][] pixelData, long timestamp) {
         this.sequenceIndex = sequenceIndex;
         this.identifier = identifier;
         this.pixelData = pixelData;
+        this.timestamp = timestamp;
     }
 }
